4f53650d60931edc1274053f8dbcdc6005ac331c
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-30 15:49:42 +0100

    added Hardhat

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -149,7 +149,7 @@ const Index = () => {
         <span className="text-sm text-gray-500 dark:text-gray-400">Entrepreneur</span>
         <div className="flex mt-4 md:mt-6">
             <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit profile</a>
-            <a href="#" className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Logout</a>
+            <a href="#" className="py-2 px-4 ms-2 text-sm font-medium  focus:outline-none  rounded-lg border   hover:text-blue-700 focus:z-10 focus:ring-4  focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 dark:hover:text-white hover:bg-gray-700">Logout</a>
         </div>
     </div>
 </div>
