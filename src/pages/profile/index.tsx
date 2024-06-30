1104edffe6df4fe39b45e562b6372829c61b9536
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-30 15:56:41 +0100

    zumji smart c

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -145,8 +145,8 @@ const Index = () => {
     </div>
     <div className="flex flex-col items-center pb-10">
         <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://i.postimg.cc/nh2FHC2T/gabe-AVATAR.jpg" alt="profileImage"/>
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">gabe.celo</h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">Entrepreneur</span>
+        <h5 className="mb-1 text-xl font-medium text-white">gabe.celo</h5>
+        <span className="text-sm text-gray-400">Entrepreneur</span>
         <div className="flex mt-4 md:mt-6">
             <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit profile</a>
             <a href="#" className="py-2 px-4 ms-2 text-sm font-medium  focus:outline-none  rounded-lg border   hover:text-blue-700 focus:z-10 focus:ring-4  focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 dark:hover:text-white hover:bg-gray-700">Logout</a>
