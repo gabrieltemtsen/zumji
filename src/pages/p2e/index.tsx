048a4e2b5be20c12972b4c2a3e37eb424b321f0e
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-11-08 18:18:18 +0100

    feat: added framer motion

--- src/pages/p2e/index.tsx
+++ src/pages/p2e/index.tsx
@@ -1,53 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { motion } from "framer-motion"; // Import Framer Motion
 import {
   Page,
   Navbar,
   Block,
   Button,
-  Link,
+  List,
+  ListItem,
+  BlockTitle,
   Preloader,
-} from "konsta/react";
-import { useAccount } from "wagmi";
-import { readContract, writeContract, waitForTransaction } from "@wagmi/core";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Layout from "../Layout";
-import { ZUMJI_ABI, ZUMJI_CONTRACT } from "@/utils/contracts";
-import { ToastContainer, toast } from "react-toastify";
+  Chip,
+} from 'konsta/react';
+import Link from 'next/link';
+import { useAccount } from 'wagmi';
+import { ZUMJI_ABI, ZUMJI_CONTRACT } from '@/utils/contracts';
+import { writeContract, readContract, waitForTransaction } from "@wagmi/core";
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useRouter } from "next/router";
+import Layout from "../Layout";
 
-const Index = () => {
+export default function Home() {
   const { address } = useAccount();
   const [inTxn, setInTxn] = useState(false);
-  const [clicks, setClicks] = useState(0);
-  const [dailyClicks, setDailyClicks] = useState(0);
-  const [zumjiPoints, setZumjiPoints] = useState(0);
-  const [hasClaimed, setHasClaimed] = useState(false);
   const [isOnboarded, setIsOnboarded] = useState(false);
-  const router = useRouter()
 
-
-  useEffect(() => {
-    const fetchClaimStatus = async () => {
-      try {
-        const claimed: any = await readContract({
-          address: ZUMJI_CONTRACT,
-          abi: ZUMJI_ABI,
-          functionName: "hasClaimedToday",
-          args: [address],
-        });
-        setHasClaimed(claimed);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (address) {
-      fetchClaimStatus();
+  const handleOnboard = async () => {
+    try {
+      setInTxn(true);
+      const tx = await writeContract({
+        address: ZUMJI_CONTRACT,
+        abi: ZUMJI_ABI,
+        functionName: "onboard",
+        args: [],
+      });
+      const hash = await waitForTransaction(tx);
+      toast.success("Onboarding successful!");
+      getIsOnboarded();
+      setInTxn(false);
+    } catch (error) {
+      toast.error("Onboarding failed!");
+      console.error("ONBOARDING ERR", error);
+      setInTxn(false);
     }
-  }, [address]);
+  };
+
   const getIsOnboarded = async () => {
     try {
       const isOnboarded: any = await readContract({
@@ -63,207 +60,133 @@ const Index = () => {
   };
 
   useEffect(() => {
-    
+    if (address) {
       getIsOnboarded();
-    
+    }
   }, [address]);
-  
-
-  useEffect(() => {
-    const now = new Date();
-    const nextMidnight = new Date(now);
-    nextMidnight.setHours(24, 0, 0, 0);
-    const timeUntilMidnight = Number(nextMidnight) - Number(now);
-
-    const timer = setTimeout(() => {
-      setDailyClicks(0);
-      setHasClaimed(false);
-    }, timeUntilMidnight);
-
-    return () => clearTimeout(timer);
-  }, [dailyClicks]);
-
-  const handleClick = () => {
-    if (dailyClicks < 100 && !hasClaimed) {
-      setClicks(clicks + 1);
-      setDailyClicks(dailyClicks + 1);
-      setZumjiPoints(zumjiPoints + 1);
-    }
-  };
-
-  const handleClaim = async () => {
-    try {
-      setInTxn(true);
-      const { hash } = await writeContract({
-        address: ZUMJI_CONTRACT,
-        abi: ZUMJI_ABI,
-        functionName: "claimDailyPoints",
-        args: [],
-      });
-      await waitForTransaction({ hash });
-      setClicks(0);
-      setHasClaimed(true);
-      toast.success("Claim successful!");
-    } catch (error) {
-      console.error(error);
-      toast.error("Claim failed!");
-    } finally {
-      setInTxn(false);
-    }
-  };
-  if(!isOnboarded) {
-    return (
-      <Layout>
-        <Navbar title={`Zumji >> Finance`} />
-        <div className="m-5 h-full">
-          <Block>
-            <div className="flex flex-wrap max-w-auto mx-auto gap-10 justify-center items-center">
-              <div className="max-w-lg w-10/12 p-6 bg-gray-800 border-gray-700 rounded-lg shadow ">
-                <Link onClick={()=>{router.push('/')}}>
-                  <h5 className="mb-2 sm:text-lg md:text-3xl font-bold tracking-tight text-white">Oops You are not onboarded, click here to do so</h5>
-                </Link>
-           
-              </div>
-            </div>
-          </Block>
-          <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
-        </div>
-        <ToastContainer />
-      </Layout>
-    );
-  }
 
   return (
     <Layout>
-      <Navbar title="Zumji >> Play2Earn" />
-      <div className="m-5">
-        <div className="max-w-sm mx-auto justify-center items-center">
-          <Block>
-            <button
-              className="flex flex-row items-center justify-center"
-              onClick={handleClick}
-              disabled={dailyClicks >= 100 || hasClaimed}
+      <Navbar title="Zumji" colors={{
+        bgIos: 'bg-black',
+        bgMaterial: 'bg-black',
+        textIos: 'text-white',
+        textMaterial: 'text-white',
+      }} />
+      <div className="h-full">
+        <section className="bg-gradient-to-b from-yellow-700/[4.79] via-yellow-800 h-full">
+          <motion.div
+            className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-12"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <a
+              href="https://celo.org"
+              target="_blank"
+              className="inline-flex items-center justify-between px-1 py-1 pr-4 text-sm rounded-full mb-7 bg-gray-800 text-white hover:bg-gray-200 hover:bg-gray-700"
+              role="alert"
             >
-              <h1 className="text-black text-lg font-bold">Press to Earn</h1>
-              <div className="button">
-                <img
-                  src="/tap.jpeg"
-                  alt="Click to Earn"
-                  className="w-40 object-cover rounded-2xl h-36 mb-2"
+              <Chip
+                media={
+                  <img
+                    alt="celo"
+                    className="ios:h-7 material:h-6 rounded-full"
+                    src="/celo.png"
+                  />
+                }
+                className="text-xs bg-black rounded-full text-white px-4 py-1.5 mr-3"
+              />{" "}
+              <span className="text-sm font-medium ml-2">Powered by Celo</span>
+              <svg
+                className="w-5 h-5 ml-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
                 />
-              </div>
-              <h1 className="text-black text-lg font-bold">Tap to Earn</h1>
-            </button>
-            <span className="text-2xl text-black font-bold m-5 p-5">
-              ZUMJI POINTS: {zumjiPoints}
-            </span>
-          </Block>
-
-          {inTxn ? (<Preloader className="center-item mt-3" />) : (
-            <Button
-              className="my-5 bg-gray-900"
-              onClick={handleClaim}
-              outline
-              disabled={clicks < 100 || inTxn || hasClaimed}
+              </svg>
+            </a>
+            <motion.h1
+              className="mb-4 text-4xl font-extrabold leading-none tracking-tight *:md:text-5xl lg:text-6xl text-white"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
             >
-              <span className="text-white">Claim Points</span>
-            </Button>
-          )}
-
-          {hasClaimed && (
-            <span className="text-red-800">
-              You have already claimed your points for today. Please come back tomorrow.
-            </span>
-          )}
-        </div>
-
-        <hr className="h-px my-2 bg-gray-700"></hr>
-
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={1}
-          loop={true}
-          autoplay={{ delay: 3000 }}
-        >
-          <SwiperSlide>
-            <div className="w-full max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
-              <div className="px-5 pb-5">
-                <a href="#">
-                  <h5 className="text-xl font-semibold tracking-tight p-5 text-white">
-                    Advertise Products Here
-                  </h5>
-                </a>
-                <div className="flex items-center mt-2.5 mb-5">
-                  <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                    <svg
-                      className="w-4 h-4 text-yellow-300"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-yellow-300"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                      className="w-4 h-4 text-gray-600"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 22 20"
-                    >
-                      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                  </div>
-                  <span className="bg-blue-100 text-xs font-semibold px-2.5 py-0.5 rounded bg-blue-200 text-blue-800 ms-3">
-                    5.0
-                  </span>
-                </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-3xl font-bold text-white">$599</span>
-                  <a
-                    href="#"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              Transform Your Business with Zumji: Loyalty, Rewards, and More
+            </motion.h1>
+            <motion.p
+              className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.2 }}
+            >
+              Empowering SMEs with Staking, Borrowing, and Innovative Rewards on Celo 
+            </motion.p>
+            <motion.div
+              className="flex mb-8 align-center justify-center space-x-4 lg:mb-16 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.4 }}
+            >
+              {isOnboarded ? (
+                <Link
+                  className="mt-6 max-w-md w-3/3 inline-flex justify-center items-center gap-x-1 text-center bg-gray-600 shadow-2xl shadow-transparent hover:shadow-black-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800 mx-5"
+                  href="/finance"
+                >
+                  Zumji Stats
+                  <svg
+                    className="w-2.5 h-2.5"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
                   >
-                    Go To
-                  </a>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-          </SwiperSlide>
-        </Swiper>
+                    <path
+                      d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                </Link>
+              ) : (
+                <span
+                  className="mt-6 max-w-md cursor-pointer w-3/3 inline-flex justify-center items-center gap-x-1 text-center bg-gray-600 shadow-2xl shadow-transparent hover:shadow-black-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800 mx-5"
+                  onClick={handleOnboard}
+                >
+                  {inTxn ? (
+                    <Preloader className="center-item mt-3" /> 
+                  ) : (
+                    <>
+                      Join Zumji
+                      <svg
+                        className="w-2.5 h-2.5"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                      >
+                        <path
+                          d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                        />
+                      </svg>
+                    </>
+                  )}
+                </span>
+              )}
+            </motion.div>
+          </motion.div>
+        </section>
       </div>
       <ToastContainer />
     </Layout>
   );
-};
-
-export default Index;
+}
