8320062aed0b035a12183816a8a03888df520f3b
numdinkushi <numdinkushi@gmail.com>
2025-01-09 15:21:41 +0100

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   const { address } = useAccount();
   const [inTxn, setInTxn] = useState(false);
   const [isOnboarded, setIsOnboarded] = useState(false);
-  const maintenance = false;
+  const maintenance = true;
 
   const handleOnboard = async () => {
     try {
