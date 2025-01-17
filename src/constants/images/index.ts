92dacd072f8f23cfaec425c4b5e6ec641b88833f
numdinkushi <numdinkushi@gmail.com>
2025-01-17 09:29:50 +0100

    added og image

--- src/constants/images/index.ts
+++ src/constants/images/index.ts
@@ -1,2 +1,3 @@
 export const homeBackground = '/images/homeBackground.jpg';
 export const zumjiLogo = '/svgs/logo/zumji_logo.svg';
+export const oGImage = '/images/open_graph/open_graph_image.jpeg';
