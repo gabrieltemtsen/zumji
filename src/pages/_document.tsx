0d7514b293db6e25953db732aae5c081be3d1cc0
numdinkushi <numdinkushi@gmail.com>
2025-01-17 07:43:03 +0100

    added meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable @next/next/no-title-in-document-head */
+import { zumjiLogo } from "@/constants/images";
+import { metaDataDescription } from "@/constants/texts";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Zumji</title>
+        <meta name="description" content={metaDataDescription} />
+        <link
+          rel="icon"
+          href={zumjiLogo}
+        />
+        <link
+          rel="apple-touch-icon"
+          href={zumjiLogo}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
